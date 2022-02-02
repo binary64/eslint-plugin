ec020230b96d7b4964955f4026b286dc6dda8fd7
binary64 <1680627+binary64@users.noreply.github.com>
2022-02-02 20:32:23 +0000

    fix(ci): remove dprint for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,33 +4,33 @@ module.exports = {
     node: true,
   },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'dprint'],
+  plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
-    'dprint/dprint': ['error', {
-      config: {
-        "incremental": true,
-        "useTabs": false,
-        "semiColons": "asi",
-        "quoteStyle": "preferSingle",
-        "lineWidth": 100,
-        "indentWidth": 2,
-        "binaryExpression.operatorPosition": "sameLine",
-        "excludes": [
-          "**/node_modules",
-          "**/*-lock.json"
-        ],
-        "plugins": [
-          "https://plugins.dprint.dev/typescript-0.62.1.wasm",
-          "https://plugins.dprint.dev/json-0.14.0.wasm",
-          "https://plugins.dprint.dev/markdown-0.12.1.wasm"
-        ]
-      }
-    }]
+    // 'dprint/dprint': ['error', {
+    //   config: {
+    //     "incremental": true,
+    //     "useTabs": false,
+    //     "semiColons": "asi",
+    //     "quoteStyle": "preferSingle",
+    //     "lineWidth": 100,
+    //     "indentWidth": 2,
+    //     "binaryExpression.operatorPosition": "sameLine",
+    //     "excludes": [
+    //       "**/node_modules",
+    //       "**/*-lock.json"
+    //     ],
+    //     "plugins": [
+    //       "https://plugins.dprint.dev/typescript-0.62.1.wasm",
+    //       "https://plugins.dprint.dev/json-0.14.0.wasm",
+    //       "https://plugins.dprint.dev/markdown-0.12.1.wasm"
+    //     ]
+    //   }
+    // }]
   },
   overrides: [
     {
